00facc84c4da609f7fc84b503bb10e7c9670175a
Loved-Ubuntu <36732660+Loved-Ubuntu@users.noreply.github.com>
2021-09-30 12:14:36 +0200

    Added post request to back-end

--- Frond_end_django/views.py
+++ Frond_end_django/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.views.decorators.csrf import csrf_exempt
+import json
+from Frond_end_django.ShapeComparisonHttp import send_image
 
+@csrf_exempt
 def index(request):
     template = loader.get_template('app/index.html')
     context = {}
+    if request.method == 'POST':
+        data_from_post = json.load(request)['image']
+        response = send_image(data_from_post)
+        print(response)
     return HttpResponse(template.render(context, request))
