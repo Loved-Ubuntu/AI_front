154486118429d246bb5cb14db27a15cf05b98db2
Loved-Ubuntu <36732660+Loved-Ubuntu@users.noreply.github.com>
2021-10-11 10:53:23 +0200

    Added 2 pages

--- Frond_end_django/urls.py
+++ Frond_end_django/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('settings', views.settings),
+    path('information', views.information)
 ]
--- Frond_end_django/views.py
+++ Frond_end_django/views.py
@@ -10,8 +10,19 @@ def index(request):
     context = {}
     if request.method == 'POST':
         data_from_post = json.load(request)['image']
+        data_from_post = data_from_post[22:]
         response_shape = send_image_shape(data_from_post)
         response_color = send_image_color(data_from_post)
         print("Shape is:", response_shape) #Remove this tag, and use the function to show a visual index
         print("Color is:", response_color) #Remove this tag, and use the function to show a visual index
     return HttpResponse(template.render(context, request))
+
+def settings(request):
+    template = loader.get_template('app/settings.html')
+    context = {}
+    return HttpResponse(template.render(context))
+
+def information(request):
+    template = loader.get_template('app/information.html')
+    context = {}
+    return HttpResponse(template.render(context))
